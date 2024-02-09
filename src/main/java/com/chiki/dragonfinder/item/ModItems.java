2b4080c86d9916d194485e8d59e9bdb1fdfb2fa1
JulianBiancardi <66981070+JulianBiancardi@users.noreply.github.com>
2024-02-09 14:54:08 -0300

    Add coocked fire dragon flesh

--- src/main/java/com/chiki/dragonfinder/item/ModItems.java
+++ src/main/java/com/chiki/dragonfinder/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.chiki.dragonfinder.item;
 
 import com.chiki.dragonfinder.DragonFinder;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,6 +10,13 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DragonFinder.MODID);
+
+    public static final RegistryObject<Item> COOCKED_F_DRAGON_FLESH = ITEMS.register("coocked_fire_dragon_flesh",
+            () -> new Item(
+                    new Item.Properties()
+                            .stacksTo(64)
+                            .food(new FoodProperties.Builder().nutrition(10).build())
+                            .tab(ModCreativeModeTab.CREATIVE_MODE_TAB)));
 
     public static final RegistryObject<Item> DRAGON_PEARL = ITEMS.register("dragonpearl",
             () -> new Item(
